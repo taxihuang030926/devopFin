18e1418202bc82e6191a1770642f3e94189e40bb
Tim Huang <taxihuang030926@gmail.com>
2025-01-08 10:55:59 +0800

    update

--- db/resumes.sql
+++ db/resumes.sql
@@ -4,19 +4,19 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-CREATE TABLE `resumes` (
-  `id` int(11) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `contact_info` varchar(255) NOT NULL,
-  `transport` text DEFAULT NULL,
-  `school_name` varchar(255) NOT NULL,
-  `education_level` varchar(255) NOT NULL,
-  `department` varchar(255) NOT NULL,
-  `study_status` varchar(50) NOT NULL,
-  `work_experience` varchar(255) NOT NULL,
-  `job_conditions` text DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+CREATE TABLE resumes (
+  ID VARCHAR(20) PRIMARY KEY NOT NULL,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  contact_info varchar(255) NOT NULL,
+  transport text DEFAULT NULL,
+  school_name varchar(255) NOT NULL,
+  education_level varchar(255) NOT NULL,
+  department varchar(255) NOT NULL,
+  study_status varchar(50) NOT NULL,
+  work_experience varchar(255) NOT NULL,
+  job_conditions text DEFAULT NULL,
+  created_at timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
